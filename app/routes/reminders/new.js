e03cdb9779ea7f5c5e250da260ee7604d666d1a8
gness1804 <graham.nessler@gmail.com>
2016-12-08 17:43:20 -0700

    WIP -- working on issue 4. Trying to get new item to generate in new route.

---
+++ app/routes/reminders/new.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+});
+
+// model(input) {
+//   return this.get('store').push('new', {
+//     title: input.title,
+//     date: input.date,
+//     notes: input.notes,
+//   });
+// }
