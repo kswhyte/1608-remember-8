8c285182efe4f533e9a80accc5d6f7008398dbf2
KINAN WHYTE <kinanwhyte@gmail.com>
2016-12-11 16:30:22 -0700

    Add Editing Field for a 'click-edit' reminder

--- app/components/edit-form.js
+++ app/components/edit-form.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  // store: Ember.inject.service(),
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
 
   actions: {
     editReminder(reminder) {
+      console.log('flag', reminder)
       this.get('store').findRecord('reminder', reminder.id).then((reminder) => {
         reminder.save().then((reminder) => {
           this.replaceWith('reminders.reminder', reminder.id)
