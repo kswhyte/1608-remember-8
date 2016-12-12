280977a4589fcb4731ac650316799cc9e4de142c
gness1804 <graham.nessler@gmail.com>
2016-12-11 22:19:12 -0700

    Two passing tests and date is formatted correctly
    Two tests pass: one that confirms the existence of input fields when
    user hits edit, and another that checks whether new user input actually
    shows up on edit (and that original user input also shows up). This
    commit also ensures that the edited date renders correctly. Live update
    on edit and reversion to uneditable mode when user clicks submit edits.
    
    Second edit test passes
    
    Editing mode is now properly toggleable
    
    changes name of submit edit button to save button
    
    date formats correctly on edit, but does not revert to uneditable state on submit
    
    moves action trigger to save button on edit
    
    dates now formatted correctly
    
    Adds additional part to acceptance test to ensure that original title shows up

--- app/controllers/reminders/edit.js
+++ app/controllers/reminders/edit.js
@@ -3,8 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     editReminder(title, date, notes, id) {
+      console.log('foo');
       this.get('store').findRecord('reminder', id).then((record) => {
-        record.setProperties({ title, date, notes, });
+        // reminder.date = new Date(reminder.date);
+        let newDate = new Date(date);
+        record.setProperties({ title, date: newDate, notes, });
         record.save();
       });
     }
