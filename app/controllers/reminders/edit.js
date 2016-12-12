37dda78cec05923e5118b7bcc1b882358d0fab3b
gness1804 <graham.nessler@gmail.com>
2016-12-12 14:39:14 -0700

    deletes commented out code

--- app/controllers/reminders/edit.js
+++ app/controllers/reminders/edit.js
@@ -5,7 +5,6 @@ export default Ember.Controller.extend({
     editReminder(title, date, notes, id) {
       console.log('foo');
       this.get('store').findRecord('reminder', id).then((record) => {
-        // reminder.date = new Date(reminder.date);
         let newDate = new Date(date);
         record.setProperties({ title, date: newDate, notes, });
         record.save();
