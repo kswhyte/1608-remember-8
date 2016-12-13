9250d69daa2af17d2243b77ba32c23f9ff3eaff6
gness1804 <graham.nessler@gmail.com>
2016-12-12 19:26:31 -0700

    edit and save functionality is starting to work

--- app/components/reminder-form.js
+++ app/components/reminder-form.js
@@ -4,7 +4,10 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
 
   actions: {
-    createReminder() {
+    createReminder(title, date, notes) {
+      // if (edit) {
+      //   console.log('foo');
+      // }
       let reminder = this.getProperties('title', 'date', 'notes');
       reminder.date = new Date(reminder.date);
       this.get('store').createRecord('reminder', reminder).save().then(() => {
