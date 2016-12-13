aef93728e6457e92ffb3c31ff373911f4bb0396b
gness1804 <graham.nessler@gmail.com>
2016-12-12 19:51:52 -0700

    issue is done except for rerouting and tests

--- app/components/reminder-form.js
+++ app/components/reminder-form.js
@@ -4,15 +4,21 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
 
   actions: {
-    createReminder(title, date, notes) {
-      // if (edit) {
-      //   console.log('foo');
-      // }
-      let reminder = this.getProperties('title', 'date', 'notes');
-      reminder.date = new Date(reminder.date);
-      this.get('store').createRecord('reminder', reminder).save().then(() => {
-        this.setProperties({ title: '', date: '', notes: '' });
-      });
+    createReminder(title, date, notes, id, edit) {
+      if (edit) {
+        this.get('store').findRecord('reminder', id).then((record) => {
+          let newDate = new Date(date);
+          record.setProperties({ title, date: newDate, notes, });
+          record.save();
+
+        });
+      } else {
+        let reminder = this.getProperties('title', 'date', 'notes');
+        reminder.date = new Date(reminder.date);
+        this.get('store').createRecord('reminder', reminder).save().then(() => {
+          this.setProperties({ title: '', date: '', notes: '' });
+        });
+      }
     }
   }
 });
