3b50bdb7e21043d05ee3be9c1d9b2eac8b269abb
KINAN WHYTE <kinanwhyte@gmail.com>
2016-12-13 20:16:21 -0700

    Test functionality of both indiv- and detailed-remove buttons

--- app/controllers/reminders.js
+++ app/controllers/reminders.js
@@ -3,11 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     remove(model) {
-      // console.log('test remove action', model.id)
-      // this.get('store').findRecord('reminder', model.id).then((reminder) => {
-      //   reminder.destroyRecord()
-      // });
-      // console.log(model)
       model.destroyRecord();
     }
   }
